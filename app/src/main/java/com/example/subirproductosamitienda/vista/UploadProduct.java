50c5a188361d5b7813e57a2f9ee5443dbb54b105
Apostol123 <apostol516@gmail.com>
2019-06-16 09:24:58 +0200

    before dexte permission

--- app/src/main/java/com/example/subirproductosamitienda/vista/UploadProduct.java
+++ app/src/main/java/com/example/subirproductosamitienda/vista/UploadProduct.java
@@ -1,13 +1,21 @@
 package com.example.subirproductosamitienda.vista;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.subirproductosamitienda.R;
 
@@ -15,6 +23,10 @@ import com.example.subirproductosamitienda.R;
  * A simple {@link Fragment} subclass.
  */
 public class UploadProduct extends Fragment {
+
+        private TextView seleccionarFoto;
+        private ImageView imageView;
+
 
 
     public UploadProduct() {
@@ -25,8 +37,63 @@ public class UploadProduct extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_product, container, false);
+       View view = inflater.inflate(R.layout.fragment_upload_product, container, false);
+       initData(view);
+
+        return  view;
     }
+
+    private void initData(View view) {
+            seleccionarFoto=view.findViewById(R.id.tvSeleccionarFoto);
+            imageView=view.findViewById(R.id.imagen_nuevo_porucot);
+
+            seleccionarFoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pickFromGallery();
+
+                }
+            });
+    }
+
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent,1);
+    }
+
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case 1:
+                    //data.getData return the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    // Set the Image in ImageView after decoding the String
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                    break;
+
+            }
+    }
+
+
+
 
 }
