312aa02d7e5f8d151354c03bd9bd20b0f10315c1
Apostolot <apostol516@gmail.com>
2019-06-25 08:55:59 +0200

    added the dinamic tarifas

--- app/src/main/java/com/example/subirproductosamitienda/Logica/Logica_Upload_Product.java
+++ app/src/main/java/com/example/subirproductosamitienda/Logica/Logica_Upload_Product.java
@@ -1,4 +1,164 @@
 package com.example.subirproductosamitienda.Logica;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.subirproductosamitienda.BuildConfig;
+import com.example.subirproductosamitienda.Recursos.FileProvider;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class Logica_Upload_Product {
+
+    private Fragment uploadProduct;
+    public static final  int GALLERY_REQUEST_CODE =101;
+    public static final  int CAMER_REQUEST_CODE=102;
+    private String cameraFilePath;
+
+    public String getCameraFilePath() {
+        return cameraFilePath;
+    }
+
+
+
+    public void setCameraFilePath(String cameraFilePath) {
+        this.cameraFilePath = cameraFilePath;
+    }
+
+    public Logica_Upload_Product(Fragment uploadProduct) {
+        this.uploadProduct = uploadProduct;
+    }
+
+    public Fragment getUploadProduct() {
+        return uploadProduct;
+    }
+
+    public void setUploadProduct(Fragment uploadProduct) {
+        this.uploadProduct = uploadProduct;
+    }
+
+    public void dexterPermission() {
+        Dexter.withActivity(this.getUploadProduct().getActivity())
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()){
+
+
+                }
+                if (report.isAnyPermissionPermanentlyDenied()){
+                    showSettingsDialog();
+
+                }
+            }
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).onSameThread().check();
+    }
+
+
+
+    /**
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     * NOTE: Keep proper title and message depending on your app
+     */
+  public void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getUploadProduct().getActivity());
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+    }
+
+    // navigating user to app settings
+    public void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", this.getUploadProduct().getActivity().getPackageName(), null);
+        intent.setData(uri);
+      this.getUploadProduct().getActivity().startActivityForResult(intent, 103);
+    }
+
+   public void captureFromCamera() {
+        try {
+            System.out.println("capture CAmera");
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this.getUploadProduct().getContext(), BuildConfig.APPLICATION_ID + ".Recursos.FileProvider", createImageFile()));
+            this.getUploadProduct().getActivity().startActivityForResult(intent, CAMER_REQUEST_CODE);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        this.getUploadProduct().getActivity().startActivityForResult(intent,GALLERY_REQUEST_CODE);
+    }
+
+
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //This is the directory in which the file will be created. This is the default location of Camera photos
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for using again
+        cameraFilePath = "file://" + image.getAbsolutePath();
+        return image;
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/subirproductosamitienda/vista/UploadProduct.java
+++ app/src/main/java/com/example/subirproductosamitienda/vista/UploadProduct.java
@@ -4,27 +4,39 @@ package com.example.subirproductosamitienda.vista;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.subirproductosamitienda.BuildConfig;
+import com.example.subirproductosamitienda.Logica.Logica_Upload_Product;
+import com.example.subirproductosamitienda.MainActivity;
 import com.example.subirproductosamitienda.R;
 import com.example.subirproductosamitienda.Recursos.FileProvider;
 import com.karumi.dexter.Dexter;
@@ -42,6 +54,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import static com.example.subirproductosamitienda.Logica.Logica_Upload_Product.CAMER_REQUEST_CODE;
+import static com.example.subirproductosamitienda.Logica.Logica_Upload_Product.GALLERY_REQUEST_CODE;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -50,10 +65,23 @@ public class UploadProduct extends Fragment {
         private TextView seleccionarFoto;
         private TextView tomarFoto;
         private ImageView imageView;
-        private String cameraFilePath;
-        private static final  int GALLERY_REQUEST_CODE =101;
-    private static final  int CAMER_REQUEST_CODE=102;
+        private Logica_Upload_Product logica_upload_product;
+        private EditText editTextPrecioProducto;
+        private CheckBox tarifas;
+        private EditText editTextTarifasPrducto;
+        private View lineaTarifas;
+        private LinearLayout mRootLinearLayout;
+        private EditText editTextPrecioTarifas;
+        private EditText precioTarifasPersonalizdas;
+        private TextWatcher textWatcherTarifas;
+        private   LinearLayout linearLayoutTarifas;
+    private   LinearLayout linearLayoutPreciosTarifas;
+
     private Bitmap mImageBitmap;
+    private EditText getEditTextPrecioTarifas;
+
+
+
 
     public UploadProduct() {
         // Required empty public constructor
@@ -65,7 +93,9 @@ public class UploadProduct extends Fragment {
                              Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_upload_product, container, false);
        initData(view);
-       dexterPermission();
+       logica_upload_product=new Logica_Upload_Product(this);
+
+       logica_upload_product.dexterPermission();
 
         return  view;
     }
@@ -74,11 +104,19 @@ public class UploadProduct extends Fragment {
             seleccionarFoto=view.findViewById(R.id.tvSeleccionarFoto);
             imageView=(ImageView) view.findViewById(R.id.ivfotoProducto);
             tomarFoto=view.findViewById(R.id.tvtomar);
+            tarifas=(CheckBox)view.findViewById(R.id.checkbtnTarifasProducto);
+            editTextTarifasPrducto=(EditText)view.findViewById(R.id.etTarifasProducto);
+            lineaTarifas=(View)view.findViewById(R.id.linea_tarifas);
+            mRootLinearLayout = (LinearLayout) view.findViewById(R.id.rootLinearLayout);
+            linearLayoutPreciosTarifas=(LinearLayout)view.findViewById(R.id.layout_precios_tarifas);
+            editTextPrecioProducto=(EditText)view.findViewById(R.id.etPrecioProducto);
+
 
             seleccionarFoto.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                 pickFromGallery();
+
+                 logica_upload_product.pickFromGallery();
 
 
                 }
@@ -87,7 +125,24 @@ public class UploadProduct extends Fragment {
             tomarFoto.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    captureFromCamera();
+                   logica_upload_product.captureFromCamera();
+                }
+            });
+
+
+            tarifas.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if (b){
+
+                        onAddFieldNrTarifas(view);
+
+                    }else {
+
+                            onRemoveView(view);
+
+
+                    }
                 }
             });
     }
@@ -100,149 +155,175 @@ public class UploadProduct extends Fragment {
 
         // Result code is RESULT_OK only if the user selects an Image
 
-            switch (requestCode){
-                case GALLERY_REQUEST_CODE:
-                    System.out.println("wen Gallery");
-                    //data.getData return the content URI for the selected Image
-                    Uri selectedImage = data.getData();
-                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                    // Get the cursor
-                    Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                    // Move to first row
-                    cursor.moveToFirst();
-                    //Get the column index of MediaStore.Images.Media.DATA
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    //Gets the String value in the column
-                    String imgDecodableString = cursor.getString(columnIndex);
-                    cursor.close();
-                    // Set the Image in ImageView after decoding the String
+        switch (requestCode){
+            case GALLERY_REQUEST_CODE:
+                System.out.println("wen Gallery");
+                //data.getData return the content URI for the selected Image
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                // Get the cursor
+                Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+                //Get the column index of MediaStore.Images.Media.DATA
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                //Gets the String value in the column
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                // Set the Image in ImageView after decoding the String
 
-                    imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
-                    if (imageView.getDrawable()!=null){
-                        imageView.setVisibility(View.VISIBLE);
-                    }
-                    Toast.makeText(getContext(),imageView.getDrawable().toString(),Toast.LENGTH_LONG).show();
-                    break;
-
-
-                case CAMER_REQUEST_CODE:
-                    try {
-                        Toast.makeText(getContext(),"CAMERA",Toast.LENGTH_SHORT).show();
-                        mImageBitmap = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), Uri.parse(cameraFilePath));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    imageView.setImageBitmap(mImageBitmap);
-                    if (imageView.getDrawable()!=null){
-                        imageView.setVisibility(View.VISIBLE);
-                    }
-
-                    break;
-
-            }
-    }
-
-    public void dexterPermission() {
-        Dexter.withActivity(this.getActivity())
-                .withPermissions(
-                        Manifest.permission.CAMERA,
-                        Manifest.permission.INTERNET,
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ).withListener(new MultiplePermissionsListener() {
-            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
-                if (report.areAllPermissionsGranted()){
-
-
+                imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                if (imageView.getDrawable()!=null){
+                    imageView.setVisibility(View.VISIBLE);
                 }
-                if (report.isAnyPermissionPermanentlyDenied()){
-                    showSettingsDialog();
+                Toast.makeText(getContext(),imageView.getDrawable().toString(),Toast.LENGTH_LONG).show();
+                break;
 
+
+            case CAMER_REQUEST_CODE:
+                try {
+                    Toast.makeText(getContext(),"CAMERA",Toast.LENGTH_SHORT).show();
+                    mImageBitmap = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), Uri.parse(logica_upload_product.getCameraFilePath()));
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }
-            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-                token.continuePermissionRequest();
-            }
-        }).onSameThread().check();
-    }
+                imageView.setImageBitmap(mImageBitmap);
+                if (imageView.getDrawable()!=null){
+                    imageView.setVisibility(View.VISIBLE);
+                }
 
+                break;
 
-
-    /**
-     * Showing Alert Dialog with Settings option
-     * Navigates user to app settings
-     * NOTE: Keep proper title and message depending on your app
-     */
-    private void showSettingsDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
-        builder.setTitle("Need Permissions");
-        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
-        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-                openSettings();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        builder.show();
-
-    }
-
-    // navigating user to app settings
-    private void openSettings() {
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts("package", this.getActivity().getPackageName(), null);
-        intent.setData(uri);
-        this.getActivity().startActivityForResult(intent, 101);
-    }
-
-    private void captureFromCamera() {
-        try {
-            System.out.println("capture CAmera");
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this.getContext(), BuildConfig.APPLICATION_ID + ".Recursos.FileProvider", createImageFile()));
-            this.getActivity().startActivityForResult(intent, CAMER_REQUEST_CODE);
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
     }
 
-    private void pickFromGallery(){
-        //Create an Intent with action as ACTION_PICK
-        Intent intent=new Intent(Intent.ACTION_PICK);
-        // Sets the type as image/*. This ensures only components of type image are selected
-        intent.setType("image/*");
-        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
-        String[] mimeTypes = {"image/jpeg", "image/png"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
-        // Launching the Intent
-        this.getActivity().startActivityForResult(intent,GALLERY_REQUEST_CODE);
+
+
+
+    private TextWatcher setTextChangeListeners(View view){
+        TextWatcher textWatcherTarifas = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().equals("")) {
+                    int nr_tarifas = Integer.parseInt(editable.toString());
+
+                    System.out.println("tarifas " + nr_tarifas);
+                    for (int i = 0; i < nr_tarifas; i++) {
+
+                        editTextPrecioTarifas = new EditText(getActivity());
+                        editTextPrecioTarifas.setHint("Tarifa nr: " + (i+1));
+                        editTextPrecioTarifas.setHintTextColor(Color.WHITE);
+                        editTextPrecioTarifas.setTextColor(Color.WHITE);
+                        linearLayoutPreciosTarifas.addView(editTextPrecioTarifas);
+
+
+                        View linea = new View(getActivity());
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+                        linea.setLayoutParams(params);
+                        linea.setBackgroundColor(Color.WHITE);
+                        linearLayoutPreciosTarifas.addView(linea);
+                    }
+                }else {
+                    linearLayoutPreciosTarifas.removeAllViews();
+                }
+
+
+
+            }
+        };
+
+
+        return textWatcherTarifas;
+
     }
 
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        //This is the directory in which the file will be created. This is the default location of Camera photos
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), "Camera");
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for using again
-        cameraFilePath = "file://" + image.getAbsolutePath();
-        return image;
+    private TextWatcher setTextChangeListenersTarifas(View view) {
+        TextWatcher textWatcherTarifas = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+
+
+                    precioTarifasPersonalizdas = new EditText(getActivity());
+                    precioTarifasPersonalizdas .setHint("Precio tarifa : " + editable.toString());
+                    precioTarifasPersonalizdas .setHintTextColor(Color.WHITE);
+                    precioTarifasPersonalizdas .setTextColor(Color.WHITE);
+                    linearLayoutPreciosTarifas.addView(  precioTarifasPersonalizdas );
+
+                    View linea = new View(getActivity());
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+                    linea.setLayoutParams(params);
+                    linea.setBackgroundColor(Color.WHITE);
+                    linearLayoutPreciosTarifas.addView(linea);
+
+
+            }
+
+
+        };
+        return textWatcherTarifas;
     }
 
+
+    public void onAddFieldNrTarifas(View view) {
+        editTextPrecioProducto.setVisibility(View.INVISIBLE);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.layout_tarifas);
+        editTextPrecioTarifas = new EditText(this.getActivity());
+        editTextPrecioTarifas.setHint("Indique el numero de tarifas");
+        editTextPrecioTarifas.setTextColor(Color.WHITE);
+        linearLayout.addView(editTextPrecioTarifas);
+
+        View linea=new View(this.getActivity());
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT,1);
+        linea.setLayoutParams(params);
+      linea.setBackgroundColor(Color.WHITE);
+        linearLayout.addView(linea);
+
+        editTextPrecioTarifas.addTextChangedListener(setTextChangeListeners(view));
+
+
+    }
+
+    public void onAddField(View view) {
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.layout_tarifas);
+        EditText editText = new EditText(this.getActivity());
+        editText.setText("tarifa");
+        linearLayout.addView(editText);
+    }
+
+
+        public void onRemoveView(View view){
+            LinearLayout linearLayout=view.findViewById(R.id.layout_tarifas);
+           linearLayout.removeAllViews();
+          linearLayoutPreciosTarifas.removeAllViews();
+            editTextPrecioProducto.setVisibility(View.VISIBLE);
+
+
+
+        }
 
 
 
